95da8a5f3e760ce3ecee3cc68a729a365ffbb8a7
AmarOk <blin.sebastien@enconn.fr>
2015-12-10 14:58:27 +0100

    update

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -9,20 +9,24 @@ int main()
   BinTree *test3 = BinTree::cons(test, test2);
   BinTree* t = BinTree::NIL;
 
-  std::cout << *test << std::endl;
-  std::cout << *test2 << std::endl;
-  std::cout << *test3 << std::endl;
-  std::cout << *BinTree::hd(test3) << std::endl;
-  std::cout << *BinTree::tl(test3) << std::endl;
-  std::cout << *t << std::endl;
+  std::cout << "test " << *test << std::endl;
+  std::cout << "test2 " << *test2 << std::endl;
+  std::cout << "test3 " << *test3 << std::endl;
+  std::cout << "hd(test) " << *BinTree::hd(test3) << std::endl;
+  std::cout << "tl(test) " << *BinTree::tl(test3) << std::endl;
+  std::cout << "t " << *t << std::endl;
 
  if(BinTree::isNIL(BinTree::tl(t)))
-     printf("ok");
+     printf("tl(NIL) = NIL\n");
 
- if(BinTree::toBool(t))
-     printf("NOK");
- else
-     printf("Ok");
+ if(BinTree::isNIL(BinTree::hd(t)))
+      printf("hd(NIL) = NIL\n");
+
+ if(!BinTree::toBool(t))
+     printf("NIL = false\n");
+
+ if(BinTree::toBool(test))
+      printf("!NIL = true\n");
 
   return 0;
 }
